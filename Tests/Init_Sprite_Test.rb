f5e1aac914b0605fd4f010a01b2c9145e4362147
Sebastian Meinberger <sebastian.meinberger@rwth-aachen.de>
2024-07-17 18:18:15 +0200

    Test that loads a bg sprite and then terminates after 5s

---
+++ Tests/Init_Sprite_Test.rb
@@ -0,0 +1,27 @@
+module Test_Suite
+  class Init_Sprite_Test < ITest_Scene
+    
+    @@uuid = "Init_Sprite_Test".freeze
+
+    def test_screen
+      viewport = Viewport.new(0, 0, Graphics.width, Graphics.height) 
+      background = Sprite.new viewport
+      background.bitmap = pbBitmap("Tests/test_scene")
+      wait (Graphics.frame_rate * 10)
+    end
+
+    def main
+      Graphics.transition 0
+      test_screen
+      Graphics.freeze()
+      $scene = nil
+    end
+   
+    def wait frames 
+      frames.times do
+        Graphics.update
+        Input.update
+      end
+    end
+  end
+end
