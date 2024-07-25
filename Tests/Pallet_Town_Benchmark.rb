22ec4f46895260a2f1efeb29b02ac88e0988a384
Sebastian Meinberger <sebastian.meinberger@rwth-aachen.de>
2024-07-25 09:07:28 +0200

    Added loadtime as metric

--- Tests/Pallet_Town_Benchmark.rb
+++ Tests/Pallet_Town_Benchmark.rb
@@ -3,20 +3,23 @@ module Test_Suite
     @@uuid = "Pallet_Town_Benchmark".freeze
 
     def main
-      initialise_generic_game_values       
-      
+      initialise_generic_game_values                   
       $MapFactory = PokemonMapFactory.new 42
-      clear_all_events $game_map
-      $game_player.moveto($data_system.start_x, $data_system.start_y)
+      clear_all_events $game_map      
+      $game_player.moveto(19, 5)
       $PokemonEncounters = PokemonEncounters.new
       $PokemonEncounters.setup($game_map.map_id)
-      $game_map.autoplay
-      $game_map.update
-
+      Events.onMapSceneChange += @@signal_loading_finised
+      walk_up_and_down 12
     end
 
     #privat
-    
+    @@signal_loading_finised = -> (s,e){
+      path = ARGV[0] + "/Raw_Measurments/loadtime"
+      puts path
+      File.write(path, "loaded\n", mode: "a")
+    }
+
     def initialise_generic_game_values
       pbMapInterpreter&.setup(nil, 0, 0)
       $scene = Scene_Map.new
@@ -35,6 +38,14 @@ module Test_Suite
     
     def clear_all_events map
       map.events.clear
+    end
+
+    def walk_up_and_down range
+      route = RPG::MoveRoute.new
+      route.repeat = true
+      route.list = (1..range).map {RPG::MoveCommand.new(PBMoveRoute::Forward)}
+      route.list += [RPG::MoveCommand.new(PBMoveRoute::Turn180),RPG::MoveCommand.new(0)]
+      $game_player.force_move_route(route)
     end
 
   end
