977753c10372c886dd7bd8217528ad9d0fc03cb6
Sebastian Meinberger <sebastian.meinberger@rwth-aachen.de>
2024-08-02 19:12:17 +0200

    Finished base functionality

--- Data/Scripts/051_AddOns/IntroScreen.rb
+++ Data/Scripts/051_AddOns/IntroScreen.rb
@@ -11,271 +11,177 @@ SCREENSTYLE = 1
 # 1 - FR/LG
 # 2 - R/S/E
 class Scene_Intro
-
-  alias main_old main
-
   def playIntroCinematic
     intro_sprite = Sprite.new
     intro_sprite.bitmap = Bitmap.new("Graphics/Pictures/Intro/INTRO.gif")
     pbBGMPlay("INTRO_music_cries")
-    play_bitmaps_to_end intro_sprite.bitmap
+    intro_sprite.bitmap.looping = false
+    intro_sprite.bitmap.play
+    while intro_sprite.bitmap.playing and not Input.press?(Input::C) do
+      Graphics.update
+      Input.update
+    end
     pbBGMStop
   end
-
+ 
   def main
     Graphics.transition(0)
-    # Cycles through the intro pictures
-    @skip = false
-
-    #playIntroCinematic
+    playIntroCinematic
     # Selects title screen style
     @screen = GenOneStyle.new
     # Plays the title screen intro (is skippable)
     @screen.intro
-
-    # Creates/updates the main title screen loop
-    self.update
+    closeTitle
     Graphics.freeze
   end
-
-  def update
-    ret = 0
-    loop do
-      Graphics.update
-      Input.update
-      if Input.press?(Input::DOWN) &&
-        Input.press?(Input::B) &&
-        Input.press?(Input::CTRL)
-        ret = 1
-        break
-      end
-      if Input.trigger?(Input::C)
-        ret = 2
-        break
-      end
-    end
-    case ret
-    when 1
-      closeSplashDelete(scene, args)
-    when 2
-      closeTitle
-    end
-  end
-
+ 
   def closeTitle
     # Play Pokemon cry
     pbSEPlay("Absorb2", 100, 100)
     # Fade out
     pbBGMStop(1.0)
     # disposes current title screen
-    disposeTitle
-    #clearTempFolder
+    @screen.dispose 
     # initializes load screen
     sscene = PokemonLoad_Scene.new
     sscreen = PokemonLoadScreen.new(sscene)
     sscreen.pbStartLoadScreen
   end
-
-  def closeTitleDelete
-    pbBGMStop(1.0)
-    # disposes current title screen
-    disposeTitle
-    # initializes delete screen
-    sscene = PokemonLoadScene.new
-    sscreen = PokemonLoad.new(sscene)
-    sscreen.pbStartDeleteScreen
-  end
-
-  def disposeTitle
-    @screen.dispose
-  end
 end
 
-module Animation
-  require 'matrix' 
-  # Animation curves describe how a proberty changes over time.
-  # Its points describe the value of the property at a fixed point in time 
-  # and the interpolation method dictates how the property behaves between these points.
-  # There are two implicite points, the first at (0,0) and the second at (duration,0)
-  # If other values are needed, they can simply be overwritten
-  class Animation_Curve
-        
-    def initialize property_setter, interpolation
-      @property_setter = property_setter
-      @interpolation = method interpolation
-      @points = [Vector[0,0]]
-      # The next point, that happens after the last known runtime.
-      # Don't access direcly, is updated through the getter.
-      # This is used to prevent searching the entire array everytime.
-      # Since the point array is always sorted, it is always sufficient to only look at the next point and only start seaching once that lies in the past   
-      @next_index = 0   
-    end
-
-    def add_point p
-      i = 0
-      while p[0] < @points[i][0] do 
-        i += 1
-      end
-      if p[0] == @points[i][0]
-        # New value for defined time point => replace old
-        @points[i] = p
-      else
-        # New time point. Insert after the first one earlier in time 
-        @points.insert i+1, p
-      end
-    end
-
-    def next_point runtime
-      if runtime <= @points[@next_index][0]
-        return @points[@next_index]
-      else
-        # Update index, so we don't have to search the entire array later on
-        # TODO Obviously fails, if more than one point slip into the past on one update
-        @next_index = (@next_index+1).clamp(0,@points.length - 1)
-        return @points[@next_index]
-      end
-    end
-
-    def previous_point runtime
-      next_point runtime
-      index = (@next_index - 1).clamp(0,@points.length - 1)
-      return @points[index] 
-    end
-
-    def apply runtime
-      _next_point = next_point runtime
-      _previous_point = previous_point runtime
-      if runtime == _next_point[0]
-        value = _next_point[1]
-      else
-        value = @interpolation.call _previous_point, _next_point, runtime
-      end
-      @property_setter.call value
-    end
-
-    def linear_interpolation p_1, p_2, runtime
-      # Clamp, this function is only meant for interpolating, not extrapolating
-      time_point = runtime.clamp p_1[0], p_2[0]
-      m = (p_2[1]-p_1[1])/(p_2[0]-p_1[0])
-      b = p_1[1]-m*p_1[0]
-      return m*time_point + b 
-    end 
-
-
-  end
-  
-
-  class Animated_Sprite < Sprite
-    attr_accessor :curves
-    
-    def initialize *args
-     @curves = []
-     @runtime = 0
-     super(*args)
-    end
-
-    
-    def update
-      curves.each {|c| c.apply @runtime}
-      @runtime += Graphics.delta
-      self.visible = true
-      super
-    end
-
-  end
-end
 
 
 #===============================================================================
 # Styled to look like the FRLG games
 #===============================================================================
 class GenOneStyle
-  require 'matrix'
-
-    
   def initialize
-
     @sprites = {}
-    @z_min = 0
-    @z_max = 1
-
-    Kernel.pbDisplayText("Keybindings: F1", 80, 0, 99999)
-    Kernel.pbDisplayText("Version " + Settings::GAME_VERSION_NUMBER, 254, 308, 99999)
-
     @maxPoke = 140 #1st gen, pas de legend la premiere fois, graduellement plus de poke
     @customPokeList = getCustomSpeciesList(false)
     #Get random Pokemon (1st gen orandPokenly, pas de legend la prmeiere fois)
-
-    # sound file for playing the title screen BGM
-    bgm = "Pokemon Red-Blue Opening"
-    @skip = false
-    # speed of the effect movement
     @disposed = false
-    pbBGMPlay(bgm)
     @viewport = Viewport.new(0, 0, Graphics.width, Graphics.height)
     @viewport.z = 99998
-    #@sprites = {}
 
-    load_sprites
+    Kernel.pbDisplayText("Keybindings: F1", 80, 0, 99999)
+    Kernel.pbDisplayText("Version " + Settings::GAME_VERSION_NUMBER, 254, 308, 99999)
+    pbBGMPlay("Pokemon Red-Blue Opening")
+    
+    # Load Background stuff
+    load_sprites [:bg, "Graphics/Titles/gen1_bg"],
+      [:bars, "Graphics/Titles/gen1_bars"],
+      [:logo,"Graphics/Titles/pokelogo"]
+    # Load two pokemon, that will fuse
+    load_forground_pokemon
   end
   
-  def load_sprites
-    randPoke = getRandomCustomFusionForIntro(true, @customPokeList, @maxPoke)
-    [
-      # Background Stuff
-      [:bg, "Graphics/Titles/gen1_bg"],
-      [:bars, "Graphics/Titles/gen1_bars"],
-      [:logo,"Graphics/Titles/pokelogo"],
-      # The fusing pokemon
-      [:poke, (get_unfused_sprite_path randPoke[0])],
-      [:poke2, (get_unfused_sprite_path randPoke[1])],
-      [:fpoke, (get_fusion_sprite_path randPoke[0], randPoke[1])]
-    ].each do |name_path| 
+  def load_sprites *sprites
+    sprites.each do |name_path| 
       name, path = name_path
+      if not @sprites[name].nil?
+        @sprites[name].dispose
+      end
       @sprites[name] = Animation::Animated_Sprite.new(@viewport) 
       @sprites[name].bitmap = pbBitmap path
       @sprites[name].visible = false
     end
   end
 
-  def intro
+  def load_forground_pokemon
+    randPoke = getRandomCustomFusionForIntro(true, @customPokeList, @maxPoke)
+    load_sprites [:poke, (get_unfused_sprite_path randPoke[0])],
+      [:poke2, (get_unfused_sprite_path randPoke[1])],
+      [:fpoke, (getFusedPath randPoke[0], randPoke[1])]
+    @sprites[:poke].x = 400
+    @sprites[:poke2].x = -150
+    @sprites[:poke].y = @sprites[:poke2].y = 75 
+  end
+
+  def intro 
     Graphics.update
+    skip = false
+    wait_all = ->() {skip = Animation::Animated_Sprite.wait_until_all_finished skip}
     
-    slide = Animation::Animation_Curve.new ->(v){@sprites[:bg].x=v}, :linear_interpolation
+    # Turn the opacity of the two unfused pokemon slowly up. 
+    curve = Animation::Animation_Curve.new :opacity=,
+      :linear_interpolation, 
+      [1.6,255]
+    @sprites[:poke].add_curve curve
+    @sprites[:poke2].add_curve curve 
+    
+      
+    wait_all.call
 
-    slide.add_point Vector[0,-Graphics.width]
-    slide.add_point Vector[0.2,0]
-    @sprites[:bg].curves.append slide
+    # Background image slides in from left screen border
+    @sprites[:bg].create_curve :x=,
+        :linear_interpolation,
+        [0,-Graphics.width],
+        [0.2,0]
 
-    while true
-      @sprites[:bg].update
-      #binding.break
+    wait_all.call
+
+    # Bars slide in from the right screen border
+    @sprites[:bars].create_curve :x=,
+      :linear_interpolation,
+      [0,Graphics.width],
+      [0.2,0]
+
+    wait_all.call
+       
+    # Logo appears and goes from purly white to its real colors
+    ->(l=@sprites[:logo]) {
+      l.x, l.y = 50, -20 
+      l.create_curve -> (o,v){o.tone = Tone.new v, v, v},
+      :linear_interpolation,
+      [0,255], # 255 on all color channels of tone => Sprite color is shifted to 100% white
+      [0.4,0] # all color channels empty => Sprite color remains unaltered
+    }.call
+
+    wait_all.call
+    
+    while not skip do
+      # If not updated, the sliding can lag, depending on how long the download of the sprites took
       Graphics.update
+      # Both pokemon slide towards eachother
+      p1_x = @sprites[:poke].x
+      p2_x = @sprites[:poke2].x
+      dir_vec = (p2_x - p1_x)
+      @sprites[:poke].create_curve :x=,
+        :linear_interpolation,
+        [0,p1_x],
+        [6, p1_x + dir_vec/3.0], # 6 seconds for the first 2/3
+        [7.5, p1_x + dir_vec/2.0] # double speed => 1.5 seconds for last 1/3
+      @sprites[:poke2].create_curve :x=,
+        :linear_interpolation,
+        [0,p2_x],
+        [6,p2_x - dir_vec/3.0],
+        [7.5,p2_x - dir_vec/2.0]
+
+      # Shine when accalerating
+      curve = Animation::Animation_Curve.new -> (o,v) {o.tone = Tone.new v,v,v},
+        :linear_interpolation,
+        [6,0],
+        [7.5,255]
+      @sprites[:poke].add_curve curve
+      @sprites[:poke2].add_curve curve
+
+      wait_all.call
+
+      # Reveal fusion
+      @sprites[:poke].visible = @sprites[:poke2].visible = false  
+      @sprites[:fpoke].x, @sprites[:fpoke].y = 125, 75
+      @sprites[:fpoke].create_curve -> (o,v) {o.tone = Tone.new v,v,v},
+        :linear_interpolation,
+        [0,255],
+        [0.2,0],
+        [3,0]
+      
+      wait_all.call
+
+      # Reset
+      load_forground_pokemon
     end
-
-    ## Turn the opacity of two unfused pokemon slowly up
-    #poke1_opacity = animate_bitmap_blit @bitmaps[:poke], bg_rect, 64,
-    #  x_start: 400, y_start: 75,
-    #  opacity_start: 0, opacity_end: 256
-    #poke1_opacity.flash(nil,100)
-    #poke2_opacity = animate_bitmap_blit @bitmaps[:poke2], bg_rect, 64,
-    #  x_start: -150, y_start: 75,
-    #  opacity_start: 0, opacity_end: 256
-    #play_bitmaps_to_end poke1_opacity.bitmap, poke2_opacity.bitmap
-   
-    ## Background image slides in from left screen border
-    #bg_slide_in = animate_bitmap_blit @bitmaps[:bg], bg_rect, 8,
-    #  x_start: -bg_rect.width, x_end: 0, on_top: false
-    #play_bitmaps_to_end bg_slide_in.bitmap
-
-    ## Bars slide in from the right screen border
-    #bars_slide_in = animate_bitmap_blit @bitmaps[:bars], bg_rect, 8,
-    #  x_start: bg_rect.width, x_end: 0
-    #play_bitmaps_to_end bars_slide_in.bitmap
-
-    ## Logo appears and goes form purly white to its real colors
-    #logo
-
   end
 
   def getFusedPath(randpoke1, randpoke2)
@@ -301,23 +207,3 @@ end
 def disposed?
   return @disposed
 end
-
-def play_bitmaps_to_end *bitmaps
-  bitmaps.each do |bitmap|
-    bitmap.looping = false # Just to be sure
-    bitmap.play
-  end
- 
-  at_least_one_playing = ->() do
-    bitmaps.reduce(false) {|sum,bitmap| sum or bitmap.playing}
-  end
-
-  while at_least_one_playing.call and not @skip do
-    Graphics.update
-    Input.update
-    if Input.press?(Input::C)
-      @skip = true
-    end
-  end
-end
-
