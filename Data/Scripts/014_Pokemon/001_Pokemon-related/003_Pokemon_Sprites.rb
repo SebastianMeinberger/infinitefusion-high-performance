4ea5bc9f6f6492f6283c55c14b6ef0211331f4cd
Sebastian Meinberger <sebastian.meinberger@rwth-aachen.de>
2024-08-04 16:59:13 +0200

    Party icons are now animated via animation module

--- Data/Scripts/014_Pokemon/001_Pokemon-related/003_Pokemon_Sprites.rb
+++ Data/Scripts/014_Pokemon/001_Pokemon-related/003_Pokemon_Sprites.rb
@@ -105,48 +105,64 @@ end
 #===============================================================================
 # Pokémon icon (for defined Pokémon)
 #===============================================================================
-class PokemonIconSprite < SpriteWrapper
+class PokemonIconSprite < Animation::Animated_Sprite
   attr_accessor :selected
   attr_accessor :active
   attr_reader :pokemon
 
   def initialize(pokemon, viewport = nil)
     super(viewport)
+    
+
     @selected = false
     @active = false
-    @numFrames = 0
-    @currentFrame = 0
-    @counter = 0
     self.pokemon = pokemon
-    @logical_x = 0 # Actual x coordinate
-    @logical_y = 0 # Actual y coordinate
-    @adjusted_x = 0 # Offset due to "jumping" animation in party screen
-    @adjusted_y = 0 # Offset due to "jumping" animation in party screen
+    @offset_x = 0
+    @offset_y = 0
+    @base_x = 0
+    @base_y = 0
+
+    # Jump up and down animation
+    end_time = 0.25
+    if @pokemon.hp <= @pokemon.totalhp / 4;
+      end_time = 1 # Red HP - 1 second
+    elsif @pokemon.hp <= @pokemon.totalhp / 2;
+      end_time =  0.5 # Yellow HP - 0.5 seconds
+    end
+    end_time *= 0.5
+    self.create_curve :offset_y=, :linear_interpolation, [0, 0], [end_time,3], looping: true
+  end
+
+  def offset_x= value
+    @offset_x = value
+    update_position
+  end
+
+  def offset_y= value
+    @offset_y = value
+    update_position
+  end
+
+  def base_x= value
+    @base_x = value
+    update_position
+  end
+
+  def base_y= value
+    @base_y = value
+    update_position
+  end
+
+  def update_position
+    self.x = @base_x + @offset_x
+    self.y = @base_y + @offset_y
   end
 
   def dispose
     @animBitmap.dispose if @animBitmap
     super
   end
-
-  def x
-    return @logical_x;
-  end
-
-  def y
-    return @logical_y;
-  end
-
-  def x=(value)
-    @logical_x = value
-    super(@logical_x + @adjusted_x)
-  end
-
-  def y=(value)
-    @logical_y = value
-    super(@logical_y + @adjusted_y)
-  end
-
+ 
   def animBitmap=(value)
     @animBitmap = value
   end
@@ -169,12 +185,10 @@ class PokemonIconSprite < SpriteWrapper
       @animBitmap = createFusionIcon()
     end
     self.bitmap = @animBitmap.bitmap
-    self.src_rect.width = @animBitmap.height
-    self.src_rect.height = @animBitmap.height
-    @numFrames = @animBitmap.width / @animBitmap.height
-    @currentFrame = 0 if @currentFrame >= @numFrames
-    changeOrigin
+    # Icon bitmaps are all double in sice for deprecated animation reasons => only left halfe is needed
+    self.src_rect.width = self.bitmap.width/2
   end
+
   def useTripleFusionIcon(species)
     dexNum = getDexNumberForSpecies(species)
     return isTripleFusion?(dexNum)
@@ -218,80 +232,7 @@ class PokemonIconSprite < SpriteWrapper
       end
     end
     return result_icon
-  end
-
-  def setOffset(offset = PictureOrigin::Center)
-    @offset = offset
-    changeOrigin
-  end
-
-  def changeOrigin
-    return if !self.bitmap
-    @offset = PictureOrigin::TopLeft if !@offset
-    case @offset
-    when PictureOrigin::TopLeft, PictureOrigin::Left, PictureOrigin::BottomLeft
-      self.ox = 0
-    when PictureOrigin::Top, PictureOrigin::Center, PictureOrigin::Bottom
-      self.ox = self.src_rect.width / 2
-    when PictureOrigin::TopRight, PictureOrigin::Right, PictureOrigin::BottomRight
-      self.ox = self.src_rect.width
-    end
-    case @offset
-    when PictureOrigin::TopLeft, PictureOrigin::Top, PictureOrigin::TopRight
-      self.oy = 0
-    when PictureOrigin::Left, PictureOrigin::Center, PictureOrigin::Right
-      # NOTE: This assumes the top quarter of the icon is blank, so oy is placed
-      #       in the middle of the lower three quarters of the image.
-      self.oy = self.src_rect.height * 5 / 8
-    when PictureOrigin::BottomLeft, PictureOrigin::Bottom, PictureOrigin::BottomRight
-      self.oy = self.src_rect.height
-    end
-  end
-
-  # How long to show each frame of the icon for
-  def counterLimit
-    return 0 if @pokemon.fainted? # Fainted - no animation
-    # ret is initially the time a whole animation cycle lasts. It is divided by
-    # the number of frames in that cycle at the end.
-    ret = Graphics.frame_rate / 4 # Green HP - 0.25 seconds
-    if @pokemon.hp <= @pokemon.totalhp / 4;
-      ret *= 4 # Red HP - 1 second
-    elsif @pokemon.hp <= @pokemon.totalhp / 2;
-      ret *= 2 # Yellow HP - 0.5 seconds
-    end
-    ret /= @numFrames
-    ret = 1 if ret < 1
-    return ret
-  end
-
-  def update
-    return if !@animBitmap
-    super
-    @animBitmap.update
-    self.bitmap = @animBitmap.bitmap
-    # Update animation
-    cl = self.counterLimit
-    if cl == 0
-      @currentFrame = 0
-    else
-      @counter += 1
-      if @counter >= cl
-        @currentFrame = (@currentFrame + 1) % @numFrames
-        @counter = 0
-      end
-    end
-    self.src_rect.x = self.src_rect.width * @currentFrame
-    # Update "jumping" animation (used in party screen)
-    if @selected
-      @adjusted_x = 4
-      @adjusted_y = (@currentFrame >= @numFrames / 2) ? -2 : 6
-    else
-      @adjusted_x = 0
-      @adjusted_y = 0
-    end
-    self.x = self.x
-    self.y = self.y
-  end
+  end 
 end
 
 #===============================================================================
